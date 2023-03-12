db8d8c0e1f82cda8f0562de73156159901f8abb3
Yohanes <kartikaisabel@gmail.com>
2023-03-12 18:40:45 +0700

    commit

---
+++ Assets/Scripts/Counter.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+
+public class Counter : MonoBehaviour
+{
+    public BehaviorTree behaviorTree;
+    public TextMesh counterText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counterText.text = behaviorTree.GetVariable("Count").GetValue().ToString();
+    }
+}
